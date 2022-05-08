7c60472a0d829b9fdc20c976dd33fd1e7a313ff8
mercedes.f1.2021 <mercedes.f1.2021@gmail.com>
2022-05-08 19:10:28 +0200

    Added dependency junit in pom.xml Adding logger and modifying test class.

--- spring/src/main/java/com/funsoft/spring/Application.java
+++ spring/src/main/java/com/funsoft/spring/Application.java
@@ -1,9 +1,13 @@
 package com.funsoft.spring;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import javax.annotation.PostConstruct;
 //import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
@@ -12,7 +16,15 @@ public class Application {
 //    @Autowired
 //    JdbcTemplate jdbcTemplate;
 
+    public static Logger logger = LoggerFactory.getLogger(Application.class);
+
+    @PostConstruct
+    public void init() {
+        logger.info("Application started ...");
+    }
+
     public static void main(String[] args) {
+        logger.info("Application executed ...");
         SpringApplication.run(Application.class, args);
     }
 
